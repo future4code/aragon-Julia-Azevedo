a67c9aa89f3f2257335dddb032757dcbf8eccecb
julia azevedo <juliae@protonmail.com>
2022-05-23 20:11:35 -0300

    projeto labex dia 1

---
+++ modulo3/projeto-labex/projeto-labex/src/routes/Router.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from '../pages/HomePage';
+import AdminPage from '../pages/AdminPage';
+import ErrorPage from "../pages/ErrorPage";
+
+const Router = () => {
+    return (
+        
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<HomePage />} />
+                <Route path={"/admin"} element={<AdminPage />} />
+                <Route path={"*"} element={<ErrorPage />} />   
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
