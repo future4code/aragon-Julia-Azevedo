65deac2716661e8e0180f4aa7a187fa5bf8d57e9
julia azevedo <juliae@protonmail.com>
2022-07-11 21:55:21 -0300

    exercicios de sql finalizados

---
+++ modulo4/intro-sql/4.sql
@@ -0,0 +1,9 @@
+INSERT INTO `Funcionarios` (id, nome, email)
+VALUES ("004", "Yuzo", "yuzo@lbn.com");
+
+SELECT * FROM `Funcionarios`
+
+DELETE FROM `Funcionarios`
+WHERE id = "004"
+
+SELECT * FROM `Funcionarios`
