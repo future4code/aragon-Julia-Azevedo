1b601b62023cd50bade3e49e53df75796e4612f4
Julia <95150756+juliasbz@users.noreply.github.com>
2022-08-26 09:55:38 -0300

    Revert "Projeto lama"

--- modulo5/codigo-aula-dtos/src/router/pingRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { PingController } from '../controller/PingController'
-
-export const pingRouter = Router()
-
-const pingController = new PingController()
-
-pingRouter.get("/", pingController.ping)
--- modulo5/gabarito-projeto-lama/src/errors/UnprocessableError.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from "./BaseError";
-
-export class UnprocessableError extends BaseError {
-    constructor(
-        message: string = "Argumento inválido"
-    ) {
-        super(422, message)
-    }
-}
